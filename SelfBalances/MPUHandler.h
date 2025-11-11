639d89f5a5cd192830653281e7b6810bed777d93
Natanael Simanjuntak <natansimanjuntak17@gmail.com>
2025-11-11 13:34:29 +0700

    Edit Code

--- SelfBalances/MPUHandler.h
+++ SelfBalances/MPUHandler.h
@@ -9,14 +9,24 @@ extern float prevAngle;
 
 void initMPU() {
   mpu.initialize();
+  mpu.setXAccelOffset(-1500);
+  mpu.setYAccelOffset(200);
+  mpu.setZAccelOffset(800);
+
+  mpu.setXGyroOffset(50);
+  mpu.setYGyroOffset(-10);
+  mpu.setZGyroOffset(30);
 }
 
 float getAngle() {
-  int16_t ax, ay, az, gx, gy, gz;
+  static unsigned long lastTime = millis();
+  unsigned long now = millis();
+  float dt = (now - lastTime) / 1000.0;
+  lastTime = now;
 
+  int16_t ax, ay, az, gx, gy, gz;
   mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
-  float dt = 0.01;
   float angleAcc = atan2(ay, az) * 180.0 / PI;
   float angleGyro = prevAngle + (gx / 131.0) * dt;
   float angle = 0.98 * angleGyro + 0.02 * angleAcc;
---
+++ SelfBalances/MotorControllerCustom.h
@@ -0,0 +1,30 @@
+#ifndef MOTOR_CONTROLLER_CUSTOM_H
+#define MOTOR_CONTROLLER_CUSTOM_H
+
+#include <LMotorController.h>
+
+extern LMotorController motor;
+
+extern int ENA;
+extern int IN1;
+extern int IN2;
+extern int ENB;
+extern int IN3;
+extern int IN4;
+
+void initMotor() {
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+}
+
+void moveMotor(double power) {
+  power = constrain(power, -255, 255);
+  if (abs(power) < 10) power = 0;
+  motor.move(power, 20);
+}
+
+#endif
